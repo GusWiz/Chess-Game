8af0d05a4dbdca97f84acfc7a312466e34deb58b
Gustavo <pow10@txstate.edu>
2026-04-19 21:50:35 -0500

    Implement Bishop helper functions to check possible valid moves.

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -16,4 +16,46 @@ public class Bishop extends Piece{
         this.sheetIndex = 2;
         this.sprite = sheet.getSubimage(sheetIndex * sheetScale, isWhite ? 0 : sheetScale, sheetScale, sheetScale).getScaledInstance(board.tileSize, board.tileSize, BufferedImage.SCALE_SMOOTH);
     }
+
+    public boolean isValidMovement(int col, int row) {
+        return Math.abs(this.col - col) == Math.abs(this.row - row );
+    }
+
+    public boolean moveCollidesWithPiece(int col, int row) {
+        // check top left diagonal collision
+        if (this.col > col && this.row > row) {
+            for (int i = 1; i < Math.abs(this.col - col); i++) {
+                if(board.getPiece(this.col - i, this.row - i) != null) {
+                    return true;
+                }
+            }
+        }
+        // check top right diagonal collision
+        if (this.col < col && this.row > row) {
+            for (int i = 1; i < Math.abs(this.col - col); i++) {
+                if (board.getPiece(this.col + i, this.row - i) != null) {
+                    return true;
+                }
+            }
+        }
+
+        // check bottom left diagonal collision
+        if (this.col > col && this.row < row) {
+            for (int i = 1; i < Math.abs(this.col - col); i++) {
+                if (board.getPiece(this.col - i, this.row + i) != null) {
+                    return true;
+                }
+            }
+        }
+
+        // check bottom right diagonal collision
+        if (this.col < col && this.row < row) {
+            for (int i = 1; i < Math.abs(this.col - col); i++) {
+                if (board.getPiece(this.col + i, this.row + i) != null) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
