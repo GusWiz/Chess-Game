65e8dee743646673a10837b28ff3888f4fce4b23
Gustavo <pow10@txstate.edu>
2026-04-19 21:13:49 -0500

    Implemented rook helper functions to check if a move is valid and to follow rules.

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -22,4 +22,46 @@ public class Rook extends Piece {
         */
         this.sprite = sheet.getSubimage(sheetIndex * sheetScale, isWhite ? 0 : sheetScale, sheetScale, sheetScale).getScaledInstance(board.tileSize, board.tileSize, BufferedImage.SCALE_SMOOTH);
     }
+
+    public boolean isValidMovement(int col, int row) {
+        return this.col == col || this.row == row;
+    }
+
+    public boolean moveCollidesWithPiece(int col, int row) {
+        // check left collision
+        if (this.col > col) {
+            for (int c = this.col - 1; c > col; c--) {
+                if (board.getPiece(c, this.row) != null) {
+                    return true;
+                }
+            }
+        }
+        // check right collision
+        if (this.col < col) {
+            for (int c = this.col + 1; c < col; c++) {
+                if (board.getPiece(c,this.row) != null) {
+                    return true;
+                }
+            }
+        }
+
+        // check top collision
+        if (this.row > row) {
+            for (int r = this.row - 1; r > row; r--) {
+                if (board.getPiece(this.col, r) != null) {
+                    return true;
+                }
+            }
+        }
+
+        // check bottom collision
+        if (this.row < row) {
+            for (int r = this.row + 1; r < row; r++) {
+                if (board.getPiece(this.col, r) != null) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
